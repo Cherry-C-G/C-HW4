e6eeaee3ca18700993e7cc46bbb4712f0ec5944e
Cherry-C-G <99839382+Cherry-C-G@users.noreply.github.com>
2022-05-20 21:02:48 -0500

    Add files via upload

---
+++ MyStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharphw4
+{
+    public class MyStack<T>
+    {
+        public List<T> List { get { return List; } }
+        public int number = 0;
+
+        public int Count()
+            { return number; }
+        public T Pop()
+        {
+            if(number == 0)
+            {
+                throw new Exception("No item to pop.");
+            }T temp = List[List.Count - 1]; 
+            List.RemoveAt(List.Count-1);
+            --number;
+            return temp;
+        }
+        public void Push(T val)
+        {
+            List.Add(val);
+        }
+    }
+}
---
+++ Stack+List.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyStack<T>
+{
+    public static void Main()
+    {
+        //Create a custom Stack class MyStack<T> that can be used with any data type which has following methods
+        // 1. int Count()
+        //2.T Pop()
+        //3.Void Push()
+
+        Stack<int> numbers = new Stack<int>();
+        numbers.Push(1);
+        numbers.Push(2);
+        numbers.Push(3);
+        numbers.Push(4);
+        numbers.Push(5);
+
+
+        // A stack can be enumerated without disturbing its contents.
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.WriteLine("\nPopping '{0}'", numbers.Pop());
+        Console.WriteLine("Peek at next item to destack: {0}",
+            numbers.Peek());
+        Console.WriteLine("Popping '{0}'", numbers.Pop());
+        Console.WriteLine("Count '{0}'", numbers.Count());
+
+
+
+        // A stack can be enumerated without disturbing its contents.
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+
+        List<int> MyList = new List<int>(numbers.ToArray());
+        MyList.Add(1);
+        MyList.Add(2);
+        MyList.Add(3);
+        MyList.Add(4);
+        MyList.Add(5);
+
+        Console.WriteLine("\nAdding list:");
+        foreach (int number in MyList)
+        {
+            Console.WriteLine(number);
+        }
+        //remove
+        MyList.Remove(1);
+        MyList.Remove(2);
+        MyList.Remove(3);
+
+        Console.WriteLine("\nRemoving list:");
+        foreach (int number in MyList)
+        {
+            Console.WriteLine(number);
+        }
+
+        //InsertAt
+        MyList.Insert(1, 11);
+        MyList.Insert(2, 22);
+        MyList.Insert(3, 33);
+
+
+        Console.WriteLine("\nInserting list:");
+        foreach (int number in MyList)
+        {
+            Console.WriteLine(number);
+
+
+
+            // Create an array twice the size of the stack and copy the
+            // elements of the stack, starting at the middle of the
+            // array.
+            int[] array2 = new int[numbers.Count * 2];
+
+
+            // Create a second stack, using the constructor that accepts an
+            // IEnumerable(Of T).
+            List<int> list = new List<int>(array2);
+
+            Console.WriteLine("\nMyList.Contains(\"four\") = {0}",
+                MyList.Contains(4));
+
+            Console.WriteLine("\nMyList.Clear()");
+            MyList.Clear();
+            Console.WriteLine("\nMyList.Count = {0}", MyList.Count);
+        }
+
+
+    }
+}
+
